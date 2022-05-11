16be74e00a30cff5d276a29e34a74c02a540fc20
Jay Liu <havvyliu@gmail.com>
2022-05-11 06:39:51 -0700

    hi hi hi

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,21 @@
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
 fn main() {
-    println!("Hello, world!");
-    let stack_id: i8 = 10;
-    let null: i8;
-    let string: &str = "asdasd";
-    let heap_vector: Vec<i8> = vec![];
-    let heap_string: String = String::from("Hello");
+	let (tx, rx) = mpsc::channel();
 
-    let stack_id_ref = stack_id;
-    println!("{}", stack_id);
-    println!("{}", stack_id_ref);
-
-    // Ownership changed!!!!!!!!!!!
-    // let heap_string_ref = heap_string;
-    // Clone...
-    let heap_string_clone = heap_string.clone();
-    // Borrowing...
-    let heap_string_borrow = &heap_string;
-    println!("{}", heap_string);
-
-	let handle = thread::spawn(|| {
-		for i in 1..10 {
-			println!("number {} from spawned thread!", i);
-			thread::sleep(Duration::from_millis(1));
-		}
+	thread::spawn(move || {
+		let val = String::from("hi");
+		tx.send(val);
+		thread::sleep(Duration::from_secs(1));
+		let val = String::from("hi1");
+		tx.send(val).unwrap();
+		let val = String::from("hi2");
+		tx.send(val).unwrap();
 	});
 
-	handle.join().unwrap();
-
-	for i in 1..5 {
-		println!("number {} from main thread!", i);
-		thread::sleep(Duration::from_millis(1));
+	for received in rx {
+		println!("Got : {}", received);
 	}
-
-	
 }
